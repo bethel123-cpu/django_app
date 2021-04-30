8eb137987fa7141ce94f7008e8ecea64337618cd
Mpaogbuji bethel <bethelogbuji84@gmail.com>
2021-04-30 19:24:08 +0100

    contd

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,16 @@
-from .models import Comment
 from django import forms
+from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    content = forms.CharField(label="", widget=forms.Textarea(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Comment here !',
+            'rows': 4,
+            'cols': 50
+        }))
+
     class Meta:
         model = Comment
-        fields = ('name', 'email', 'body')
+        fields = ['content']
