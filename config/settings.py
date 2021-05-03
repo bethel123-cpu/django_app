d4ba93a213e18a2fa9caf3fd74d9ddabda725da9
Mpaogbuji bethel <bethelogbuji84@gmail.com>
2021-05-03 09:53:47 +0100

    sample

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv.__get__("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv.__get__("DEBUG")
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1', ]
 
 
 # Application definition
